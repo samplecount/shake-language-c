f5c8f3c22f2dce4a387cb1beb613d5eb61bc9c9c
Stefan Kersten <stefan@samplecount.com>
2014-01-08 11:11:50 +0100

    Put some flags returned by pkg-config into specific BuildFlags fields

--- Shakefile/C/PkgConfig.hs
+++ Shakefile/C/PkgConfig.hs
@@ -16,14 +16,51 @@ module Shakefile.C.PkgConfig (
     pkgConfig
 ) where
 
-import Data.List (isSuffixOf)
-import Shakefile.C (BuildFlags, compilerFlags, linkerFlags)
+import Data.List (isPrefixOf, isSuffixOf)
+import Shakefile.C ( BuildFlags
+                   , compilerFlags
+                   , libraries
+                   , libraryPath
+                   , linkerFlags
+                   , systemIncludes
+                   , userIncludes )
 import Shakefile.Label (append)
 import System.Exit (ExitCode(..))
 import System.Process (readProcessWithExitCode)
 
 -- ====================================================================
 -- PkgConfig
+
+-- TODO:
+--  * Use parsec or attoparsec for more robust parser
+--  * Parse preprocessor defines
+--  * Parse framework path (-F) and -framework flags
+
+parseCflags :: [String] -> (BuildFlags -> BuildFlags)
+parseCflags [] = id
+parseCflags (x:xs)
+  | isPrefixOf "-I" x = parseCflags xs . append systemIncludes [drop 2 x]
+  | isPrefixOf "-i" x = parseCflags xs . append userIncludes [drop 2 x]
+  | otherwise = append compilerFlags [(Nothing, [x])]
+
+parseLibs :: [String] -> (BuildFlags -> BuildFlags)
+parseLibs [] = id
+parseLibs (x:xs)
+  | isPrefixOf "-l" x = parseLibs xs . append libraries [drop 2 x]
+  | isPrefixOf "-L" x = parseLibs xs . append libraryPath [drop 2 x]
+  | otherwise = parseLibs xs . append linkerFlags [x]
+
+parseFlags :: String -> [String]
+parseFlags = unescape . words . head . lines
+  where
+    escape = "\\"
+    isEscaped = isSuffixOf escape
+    dropEscape = reverse . drop (length escape) . reverse
+    unescape [] = []
+    unescape [x] = [if isEscaped x then dropEscape x else x]
+    unescape (x1:x2:xs)
+      | isEscaped x1 = unescape ((dropEscape x1 ++ x2):xs)
+      | otherwise = [x1] ++ unescape (x2:xs)
 
 pkgConfig :: String -> IO (BuildFlags -> BuildFlags)
 pkgConfig pkg = do
@@ -33,15 +70,11 @@ pkgConfig pkg = do
             (code2, ldflags, _) <- readProcessWithExitCode "pkg-config" ["--libs", pkg] ""
             case code2 of
                 ExitSuccess ->
-                    return $ append compilerFlags [(Nothing, parseFlags cflags)]
-                           . append linkerFlags (parseFlags ldflags)
+                    return $ parseCflags (parseFlags cflags)
+                           . parseLibs (parseFlags ldflags)
                 _ -> notFound
         _ -> notFound
     where
         notFound = do
             putStrLn $ "WARNING: package " ++ pkg ++ " not found (pkgconfig)"
             return id
-        parseFlags = map (dropSuffix "\\") . words . head . lines
-        dropSuffix s x = if s `isSuffixOf` x
-                         then reverse (drop (length s) (reverse x))
-                         else x
