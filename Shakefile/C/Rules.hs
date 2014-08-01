a69c930865867b309ee199993cb4f0a9bad75d57
Stefan Kersten <kaoskorobase@gmail.com>
2014-08-01 18:45:15 +0200

    Replace Env arguments by build prefix FilePath
    Also remove Target argument from rule functions.

--- Shakefile/C/Rules.hs
+++ Shakefile/C/Rules.hs
@@ -12,48 +12,25 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-{-# LANGUAGE TemplateHaskell #-}
-
 module Shakefile.C.Rules (
-    Env
-  , defaultEnv
-  , buildPrefix
-  , defaultBuildPrefix
-  , executable
+    executable
   , staticLibrary
   , sharedLibrary
   , dynamicLibrary
 ) where
 
-import           Data.Char (toLower)
 import           Control.Applicative ((<$>))
 import           Control.Monad
-import           Development.Shake hiding (Env)
+import           Development.Shake
 import           Development.Shake.FilePath
 import           Development.Shake.Util (parseMakefile)
 import           Shakefile.C.BuildFlags as BuildFlags
 import           Shakefile.C.Language (languageOf)
 import           Shakefile.C.ToolChain as ToolChain
 import           Shakefile.C.Util
-import           Shakefile.Label (get, mkLabel)
+import           Shakefile.Label (get)
 import           Shakefile.SourceTree (SourceTree)
 import qualified Shakefile.SourceTree as SourceTree
-
-data Env = Env {
-    _buildPrefix :: FilePath
-  } deriving (Show)
-
-mkLabel ''Env
-
-defaultEnv :: Env
-defaultEnv = Env "."
-
-defaultBuildPrefix :: FilePath -> Target -> String -> FilePath
-defaultBuildPrefix buildDir target config =
-      buildDir
-  </> map toLower config
-  </> (platformString $ get targetPlatform target)
-  </> (archString $ get targetArch target)
 
 dependencyFile :: ToolChain -> BuildFlags -> FilePath -> [FilePath] -> FilePath -> Rules ()
 dependencyFile toolChain buildFlags input deps output = do
@@ -90,21 +67,21 @@ staticObject toolChain buildFlags input deps output = do
 sharedObject :: ObjectRule
 sharedObject toolChain = staticObject toolChain -- Disable for now: . append compilerFlags [(Nothing, ["-fPIC"])]
 
-mkObjectsDir :: Env -> FilePath -> FilePath
-mkObjectsDir env path = get buildPrefix env </> map tr (makeRelative "/" path) ++ "_obj"
+mkObjectsDir :: FilePath -> FilePath -> FilePath
+mkObjectsDir buildPrefix path = buildPrefix </> map tr (makeRelative "/" path) ++ "_obj"
     where tr '.' = '_'
           tr x   = x
 
-mkBuildPath :: Env -> FilePath -> FilePath
-mkBuildPath env path = get buildPrefix env </> makeRelative "/" path
+mkBuildPath :: FilePath -> FilePath -> FilePath
+mkBuildPath buildPrefix path = buildPrefix </> makeRelative "/" path
 
 buildProduct :: ObjectRule -> Linker -> FilePath
-             -> Env -> ToolChain
+             -> FilePath -> ToolChain
              -> SourceTree BuildFlags
              -> Rules FilePath
-buildProduct object link fileName env toolChain sources = do
-    let resultPath = mkBuildPath env fileName
-        objectsDir = mkObjectsDir env fileName
+buildProduct object link fileName buildPrefix toolChain sources = do
+    let resultPath = mkBuildPath buildPrefix fileName
+        objectsDir = mkObjectsDir buildPrefix fileName
         sources' = SourceTree.flags (get ToolChain.defaultBuildFlags toolChain) sources
     objects <- forM (SourceTree.apply BuildFlags.defaultBuildFlags sources') $ \(buildFlags, (src, deps)) -> do
         let obj = objectsDir </> makeRelative "/" (src <.> "o")
@@ -114,37 +91,37 @@ buildProduct object link fileName env toolChain sources = do
     return resultPath
 
 -- | Rule for building an executable.
-executable :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
-executable env _ toolChain name sources =
+executable :: FilePath -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+executable buildPrefix toolChain name sources =
     buildProduct
         staticObject
         (get linker toolChain Executable)
         (get linkResultFileName toolChain Executable name)
-        env toolChain sources
+        buildPrefix toolChain sources
 
 -- | Rule for building a static library.
-staticLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
-staticLibrary env _ toolChain name sources =
+staticLibrary :: FilePath -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+staticLibrary buildPrefix toolChain name sources =
     buildProduct
         staticObject
         (get archiver toolChain)
         (get archiveFileName toolChain name)
-        env toolChain sources
+        buildPrefix toolChain sources
 
 -- | Rule for building a shared library.
-sharedLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
-sharedLibrary env _ toolChain name sources =
+sharedLibrary :: FilePath -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+sharedLibrary buildPrefix toolChain name sources =
     buildProduct
         sharedObject
         (get linker toolChain SharedLibrary)
         (get linkResultFileName toolChain SharedLibrary name)
-        env toolChain sources
+        buildPrefix toolChain sources
 
 -- | Rule for building a dynamic library.
-dynamicLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
-dynamicLibrary env _ toolChain name sources =
+dynamicLibrary :: FilePath -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+dynamicLibrary buildPrefix toolChain name sources =
     buildProduct
         sharedObject
         (get linker toolChain DynamicLibrary)
         (get linkResultFileName toolChain DynamicLibrary name)
-        env toolChain sources
+        buildPrefix toolChain sources
