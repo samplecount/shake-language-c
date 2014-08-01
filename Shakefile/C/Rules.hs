251da1b1787472eae16ef80827a534ccf9bfb6a8
Stefan Kersten <kaoskorobase@gmail.com>
2014-08-01 17:34:21 +0200

    Simplify internal type signatures and fix compiler warnings

--- Shakefile/C/Rules.hs
+++ Shakefile/C/Rules.hs
@@ -26,21 +26,16 @@ module Shakefile.C.Rules (
 
 import           Control.Applicative ((<$>))
 import           Control.Monad
-import           Data.Char (toLower)
-import           Development.Shake ((?>), command_, need, readFile', systemOutput, want, writeFile')
-import qualified Development.Shake as Shake
+import           Development.Shake hiding (Env)
 import           Development.Shake.FilePath
 import           Development.Shake.Util (parseMakefile)
-import           Data.Maybe
-import           Data.Version
 import           Shakefile.C.BuildFlags as BuildFlags
 import           Shakefile.C.Language (languageOf)
 import           Shakefile.C.ToolChain as ToolChain
 import           Shakefile.C.Util
-import           Shakefile.Label ((:->), append, get, mkLabel, prepend, set)
+import           Shakefile.Label (get, mkLabel)
 import           Shakefile.SourceTree (SourceTree)
 import qualified Shakefile.SourceTree as SourceTree
-import           System.Environment (getEnvironment)
 
 data Env = Env {
     _buildPrefix :: FilePath
@@ -51,7 +46,7 @@ mkLabel ''Env
 defaultEnv :: Env
 defaultEnv = Env "."
 
-dependencyFile :: ToolChain -> BuildFlags -> FilePath -> [FilePath] -> FilePath -> Shake.Rules ()
+dependencyFile :: ToolChain -> BuildFlags -> FilePath -> [FilePath] -> FilePath -> Rules ()
 dependencyFile toolChain buildFlags input deps output = do
     output ?=> \_ -> do
         need $ [input] ++ deps
@@ -63,12 +58,10 @@ dependencyFile toolChain buildFlags input deps output = do
                 ++ compilerFlagsFor (languageOf input) buildFlags
                 ++ ["-MM", "-o", output, input]
 
--- FIXME: This function fails for paths with escaped spaces
 parseDependencies :: String -> [FilePath]
---parseDependencies = drop 2 . words . filter (/= '\\')
 parseDependencies = snd . head . parseMakefile
 
-type ObjectRule = ToolChain -> BuildFlags -> FilePath -> [FilePath] -> FilePath -> Shake.Rules ()
+type ObjectRule = ToolChain -> BuildFlags -> FilePath -> [FilePath] -> FilePath -> Rules ()
 
 staticObject :: ObjectRule
 staticObject toolChain buildFlags input deps output = do
@@ -88,21 +81,21 @@ staticObject toolChain buildFlags input deps output = do
 sharedObject :: ObjectRule
 sharedObject toolChain = staticObject toolChain -- Disable for now: . append compilerFlags [(Nothing, ["-fPIC"])]
 
-mkObjectsDir :: Env -> Target -> FilePath -> FilePath
-mkObjectsDir env target path = get buildPrefix env </> map tr (makeRelative "/" path) ++ "_obj"
+mkObjectsDir :: Env -> FilePath -> FilePath
+mkObjectsDir env path = get buildPrefix env </> map tr (makeRelative "/" path) ++ "_obj"
     where tr '.' = '_'
           tr x   = x
 
-mkBuildPath :: Env -> Target -> FilePath -> FilePath
-mkBuildPath env target path = get buildPrefix env </> makeRelative "/" path
+mkBuildPath :: Env -> FilePath -> FilePath
+mkBuildPath env path = get buildPrefix env </> makeRelative "/" path
 
 buildProduct :: ObjectRule -> Linker -> FilePath
-             -> Env -> Target -> ToolChain
+             -> Env -> ToolChain
              -> SourceTree BuildFlags
-             -> Shake.Rules FilePath
-buildProduct object link fileName env target toolChain sources = do
-    let resultPath = mkBuildPath env target fileName
-        objectsDir = mkObjectsDir env target fileName
+             -> Rules FilePath
+buildProduct object link fileName env toolChain sources = do
+    let resultPath = mkBuildPath env fileName
+        objectsDir = mkObjectsDir env fileName
         sources' = SourceTree.flags (get ToolChain.defaultBuildFlags toolChain) sources
     objects <- forM (SourceTree.apply BuildFlags.defaultBuildFlags sources') $ \(buildFlags, (src, deps)) -> do
         let obj = objectsDir </> makeRelative "/" (src <.> "o")
@@ -112,37 +105,37 @@ buildProduct object link fileName env target toolChain sources = do
     return resultPath
 
 -- | Rule for building an executable.
-executable :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Shake.Rules FilePath
-executable env target toolChain name sources =
+executable :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+executable env _ toolChain name sources =
     buildProduct
         staticObject
         (get linker toolChain Executable)
         (get linkResultFileName toolChain Executable name)
-        env target toolChain sources
+        env toolChain sources
 
 -- | Rule for building a static library.
-staticLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Shake.Rules FilePath
-staticLibrary env target toolChain name sources =
+staticLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+staticLibrary env _ toolChain name sources =
     buildProduct
         staticObject
         (get archiver toolChain)
         (get archiveFileName toolChain name)
-        env target toolChain sources
+        env toolChain sources
 
 -- | Rule for building a shared library.
-sharedLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Shake.Rules FilePath
-sharedLibrary env target toolChain name sources =
+sharedLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+sharedLibrary env _ toolChain name sources =
     buildProduct
         sharedObject
         (get linker toolChain SharedLibrary)
         (get linkResultFileName toolChain SharedLibrary name)
-        env target toolChain sources
+        env toolChain sources
 
 -- | Rule for building a dynamic library.
-dynamicLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Shake.Rules FilePath
-dynamicLibrary env target toolChain name sources =
+dynamicLibrary :: Env -> Target -> ToolChain -> String -> SourceTree BuildFlags -> Rules FilePath
+dynamicLibrary env _ toolChain name sources =
     buildProduct
         sharedObject
         (get linker toolChain DynamicLibrary)
         (get linkResultFileName toolChain DynamicLibrary name)
-        env target toolChain sources
+        env toolChain sources
