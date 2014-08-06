1a68f295b6108f6e3d01dd84878c4f82f3d13fb9
Stefan Kersten <kaoskorobase@gmail.com>
2014-08-06 18:47:30 +0200

    Change link product rules to take sources in the Action monad
    This allows to read the list of sources from config files or search
    them in a directory.

--- Shakefile/C/Rules.hs
+++ Shakefile/C/Rules.hs
@@ -19,79 +19,55 @@ module Shakefile.C.Rules (
   , dynamicLibrary
 ) where
 
-import           Control.Monad
+import           Control.Applicative ((<$>))
+import qualified Data.HashMap.Strict as Map
 import           Development.Shake
 import           Development.Shake.FilePath
 import           Shakefile.C.BuildFlags as BuildFlags
 import           Shakefile.C.ToolChain as ToolChain
-import           Shakefile.C.Util
 import           Shakefile.Label (get)
-import           Shakefile.SourceTree (SourceTree)
-import qualified Shakefile.SourceTree as SourceTree
-
-applyDefaultBuildFlags :: Monad m => ToolChain -> SourceTree m BuildFlags -> SourceTree m BuildFlags
-applyDefaultBuildFlags toolChain sources =
-  SourceTree.flags (get ToolChain.defaultBuildFlags toolChain) sources
-
-objects :: ToolChain
-        -> SourceTree Action BuildFlags
-        -> FilePath
-        -> Rules [FilePath]
-objects toolChain sources objectsDir = do
-    forM (SourceTree.flatten (applyDefaultBuildFlags toolChain sources)) $ \(getBuildFlags, (src, deps)) -> do
-        let obj = objectsDir </> makeRelative "/" (src <.> "o")
-        obj ?=> \_ -> do
-          need deps
-          buildFlags <- getBuildFlags
-          (get compiler toolChain)
-            toolChain
-            (buildFlags BuildFlags.defaultBuildFlags)
-            src
-            obj
-        return obj
 
 mkObjectsDir :: FilePath -> FilePath
 mkObjectsDir path = takeDirectory path </> map tr (takeFileName path) ++ "_obj"
     where tr '.' = '_'
           tr x   = x
 
--- | Rule for building a static library.
-staticLibrary :: ToolChain -> FilePath -> SourceTree Action BuildFlags -> Rules FilePath
-staticLibrary toolChain result sources = do
-  objs <- objects toolChain sources (mkObjectsDir result)
-  result ?=> \_ -> do
-    buildFlags <- SourceTree.collect $ applyDefaultBuildFlags toolChain sources
-    (get archiver toolChain)
+buildProduct :: Linker -> ToolChain -> FilePath -> Action (BuildFlags -> BuildFlags) -> Action [FilePath] -> Rules FilePath
+buildProduct link toolChain result getBuildFlags getSources = do
+  let objectsDir = mkObjectsDir result
+  objectsMap <- newCache $ \() -> do
+    sources <- getSources
+    return $ Map.fromList
+           $ map (\src -> (objectsDir </> makeRelative "/" (src <.> "o"), src))
+                 sources
+  cachedBuildFlags <- newCache $ \() -> getBuildFlags
+  result *> \_ -> do
+    buildFlags <- cachedBuildFlags ()
+    objs <- Map.keys <$> objectsMap ()
+    need objs
+    link
       toolChain
-      (buildFlags BuildFlags.defaultBuildFlags)
+      (buildFlags . get ToolChain.defaultBuildFlags toolChain $ BuildFlags.defaultBuildFlags)
       objs
       result
-  return result
-
-linkProduct :: LinkResult
-            -> ToolChain
-            -> FilePath
-            -> SourceTree Action BuildFlags
-            -> Rules FilePath
-linkProduct linkResult toolChain result sources = do
-  objs <- objects toolChain sources (mkObjectsDir result)
-  result ?=> \_ -> do
-    buildFlags <- SourceTree.collect $ applyDefaultBuildFlags toolChain sources
-    ((get linker toolChain) linkResult)
+  (objectsDir ++ "//*.o") *> \obj -> do
+    buildFlags <- cachedBuildFlags ()
+    Just src <- Map.lookup obj <$> objectsMap ()
+    (get compiler toolChain)
       toolChain
-      (buildFlags BuildFlags.defaultBuildFlags)
-      objs
-      result
+      (buildFlags . get ToolChain.defaultBuildFlags toolChain $ BuildFlags.defaultBuildFlags)
+      src
+      obj
   return result
 
--- | Rule for building an executable.
-executable :: ToolChain -> FilePath -> SourceTree Action BuildFlags -> Rules FilePath
-executable = linkProduct Executable
+executable :: ToolChain -> FilePath -> Action (BuildFlags -> BuildFlags) -> Action [FilePath] -> Rules FilePath
+executable toolChain = buildProduct (get linker toolChain Executable) toolChain
 
--- | Rule for building a shared library.
-sharedLibrary :: ToolChain -> FilePath -> SourceTree Action BuildFlags -> Rules FilePath
-sharedLibrary = linkProduct SharedLibrary
+staticLibrary :: ToolChain -> FilePath -> Action (BuildFlags -> BuildFlags) -> Action [FilePath] -> Rules FilePath
+staticLibrary toolChain = buildProduct (get archiver toolChain) toolChain
 
--- | Rule for building a dynamic library.
-dynamicLibrary :: ToolChain -> FilePath -> SourceTree Action BuildFlags -> Rules FilePath
-dynamicLibrary = linkProduct DynamicLibrary
+sharedLibrary :: ToolChain -> FilePath -> Action (BuildFlags -> BuildFlags) -> Action [FilePath] -> Rules FilePath
+sharedLibrary toolChain = buildProduct (get linker toolChain SharedLibrary) toolChain
+
+dynamicLibrary :: ToolChain -> FilePath -> Action (BuildFlags -> BuildFlags) -> Action [FilePath] -> Rules FilePath
+dynamicLibrary toolChain = buildProduct (get linker toolChain DynamicLibrary) toolChain
