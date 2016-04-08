02a69b1059241c3bd591975298735c666136b0e3
Stefan Kersten <stefan@samplecount.com>
2016-04-08 19:19:39 +0200

    Add copyright notice

--- src/Development/Shake/Language/C/Host/Windows.hs
+++ src/Development/Shake/Language/C/Host/Windows.hs
@@ -1,4 +1,4 @@
--- Copyright 2012-2014 Samplecount S.L.
+-- Copyright 2012-2016 Samplecount S.L.
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
