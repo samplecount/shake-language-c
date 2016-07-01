015041da2565cfcff69faeabf4b8483f96635ed4
Stefan Kersten <stefan@samplecount.com>
2016-07-01 10:43:19 +0200

    Add mkConfig function that caches dependencies
    Add a withConfig variant that cache config file dependencies dynamically
    instead of requiring them upfront.

--- src/Development/Shake/Language/C/Config.hs
+++ src/Development/Shake/Language/C/Config.hs
@@ -8,6 +8,7 @@ similar to the functions provided by "Development.Shake.Config".
 -}
 module Development.Shake.Language.C.Config(
     withConfig
+  , mkConfig
   , parsePaths
   , getPaths
 ) where
@@ -19,7 +20,7 @@ import Development.Shake.Classes
 import Development.Shake.Config (readConfigFileWithEnv)
 import Development.Shake.Language.C.Util (words')
 
-newtype Config = Config ([(String, String)], FilePath, String) deriving (Show,Typeable,Eq,Hashable,Binary,NFData)
+newtype Config = Config ([FilePath], FilePath, [(String, String)], String) deriving (Show,Typeable,Eq,Hashable,Binary,NFData)
 
 {- | Given a list of dependencies, return a function that takes an environment
 of variable bindings, a configuration file path and a configuration variable
@@ -42,11 +43,38 @@ withConfig :: [FilePath]
                      -> String
                      -> Action (Maybe String))
 withConfig deps = do
-  fileCache <- newCache $ \(env, file) -> do
+  fileCache <- newCache $ \(file, env) -> do
     need deps
     liftIO $ readConfigFileWithEnv env file
-  query <- addOracle $ \(Config (env, file, key)) -> Map.lookup key <$> fileCache (env, file)
-  return $ \env file key -> query (Config (env, file, key))
+  query <- addOracle $ \(Config (_, file, env, key)) -> Map.lookup key <$> fileCache (file, env)
+  return $ \env file key -> query (Config ([], file, env, key))
+
+{- | Return a function that takes a list of dependencies, a configuration file
+path, an environment of variable bindings and a configuration variable and
+returns the corresponding configuration value.
+
+This function is more flexible than `Development.Shake.Config.usingConfigFile`.
+It allows the use of multiple configuration files as well as specifying default
+variable bindings.
+
+Typical usage would be something like this:
+
+> -- In the Rules monad
+> getConfig <- mkConfig
+> -- Then in an Action
+> ... value <- getConfig ["some_generated_config.cfg"] [("variable", "default value")] "config.cfg" "variable"
+-}
+mkConfig :: Rules (     [FilePath]
+                     -> FilePath
+                     -> [(String,String)]
+                     -> String
+                     -> Action (Maybe String))
+mkConfig = do
+  fileCache <- newCache $ \(deps, file, env) -> do
+    need deps
+    liftIO $ readConfigFileWithEnv env file
+  query <- addOracle $ \(Config (deps, file, env, key)) -> Map.lookup key <$> fileCache (deps, file, env)
+  return $ \deps file env key -> query (Config (deps, file, env, key))
 
 -- | Parse a list of space separated paths from an input string. Spaces can be escaped by @\\@ characters.
 --
